1900d3249889abfa38b1c5ac92c25e311cdec2f0
Pedro Baleroni <pbaleroni@gmail.com>
2025-10-22 17:48:50 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/javadex/App.java
@@ -0,0 +1,36 @@
+package com.mycompany.javadex;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import classes.Pokemon;
+import classes.Tipo;
+import database.database;
+
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        var javaVersion = SystemInfo.javaVersion();
+        var javafxVersion = SystemInfo.javafxVersion();
+        
+        database bd = new database();
+        bd.inicialize();
+
+        var label = new Label(bd.buscaPokemon(25).getNome());
+        var scene = new Scene(new StackPane(label), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
