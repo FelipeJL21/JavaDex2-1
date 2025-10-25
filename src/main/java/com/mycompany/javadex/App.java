6c191baf56b4d11c568b01c2365400a9aa832ea0
Pedro Baleroni <pbaleroni@gmail.com>
2025-10-25 15:46:01 -0300

    up sum

--- src/main/java/com/mycompany/javadex/App.java
+++ src/main/java/com/mycompany/javadex/App.java
@@ -3,7 +3,7 @@ package javadex;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import classes.Pokemon;
 import classes.Tipo;
@@ -23,8 +23,21 @@ public class App extends Application {
         database bd = new database();
         bd.inicialize();
 
-        var label = new Label(bd.buscaPokemon(25).getNome());
-        var scene = new Scene(new StackPane(label), 640, 480);
+        Label pikachu = new Label(bd.buscaPokemon(25).getNome());
+        Label raichu = new Label(bd.buscaPokemon(26).getNome());
+        var label2 = new Label("Hello World");
+        StackPane pilha = new StackPane(pikachu);
+        VBox vertical = new VBox();
+        HBox horizontal = new HBox();
+        horizontal.getChildren().add(pikachu);
+        horizontal.getChildren().add(raichu);
+        horizontal.getChildren().add(label2);
+        //Hello World
+        vertical.getChildren().add(pikachu);
+        vertical.getChildren().add(horizontal);
+        
+        //Pikachu Raichu (Horizontal)
+        var scene = new Scene(vertical,  640, 480);
         stage.setScene(scene);
         stage.show();
     }
